168fe5853d65684b07a51e05f748bf6ca40ee16b
TheVanceReport <ardenkvance@gmail.com>
2026-09-17 20:20:50 -0500

    Update quotes.js

--- netlify/functions/quotes.js
+++ netlify/functions/quotes.js
@@ -2,19 +2,42 @@ exports.handler = async function(event, context) {
 
     try {
 
-        const symbol = "WOLF";
+        const symbols = [
+            "BMEA",
+            "SEDG",
+            "PAGS",
+            "RUN",
+            "LUMN",
+            "PLUG",
+            "WOLF",
+            "FSRN",
+            "UPST",
+            "CHWY"
+        ];
 
-        const response = await fetch(
-            `https://api.twelvedata.com/quote?symbol=${symbol}&apikey=${process.env.STOCK_API_KEY}`
-        );
+        const results = [];
 
+        for (const symbol of symbols) {
 
-        const data = await response.json();
+            const response = await fetch(
+                `https://api.twelvedata.com/quote?symbol=${symbol}&apikey=${process.env.STOCK_API_KEY}`
+            );
+
+            const data = await response.json();
+
+            results.push({
+                symbol: symbol,
+                price: Number(data.close),
+                previousClose: Number(data.previous_close),
+                change: Number(data.change),
+                percentChange: Number(data.percent_change)
+            });
+        }
 
 
         return {
             statusCode: 200,
-            body: JSON.stringify(data)
+            body: JSON.stringify(results)
         };
 
 
