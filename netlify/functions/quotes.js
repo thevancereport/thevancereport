e36efa08f7dc241d4ce10ba95a7ddbcc7b06380e
TheVanceReport <ardenkvance@gmail.com>
2026-09-17 19:52:55 -0500

    Update quotes.js

--- netlify/functions/quotes.js
+++ netlify/functions/quotes.js
@@ -1,10 +1,32 @@
 exports.handler = async function(event, context) {
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "Quotes function is working!"
-        })
-    };
+    try {
+
+        const symbol = "WOLF";
+
+        const response = await fetch(
+            `https://api.twelvedata.com/quote?symbol=${symbol}&apikey=${process.env.STOCK_API_KEY}`
+        );
+
+
+        const data = await response.json();
+
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        };
+
+
+    } catch (error) {
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: error.message
+            })
+        };
+
+    }
 
 };
